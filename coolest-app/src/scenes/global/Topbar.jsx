5fa8dfd5bdf7dba821e65a5ff7c6e93790df7905
savagekyle <66024623+savagekyle@users.noreply.github.com>
2023-03-21 15:52:07 -0400

    implemented light and dark theme

---
+++ coolest-app/src/scenes/global/Topbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box>
+      <IconButton onClick={colorMode.toggleColorMode}>
+        {theme.palette.mode === "dark" ? (
+          <DarkModeOutlinedIcon />
+        ) : (
+          <LightModeOutlinedIcon />
+        )}
+      </IconButton>
+    </Box>
+  );
+};
+
+export default Topbar;
