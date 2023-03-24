08daaec8f41ec0129c1fa90195645314f784e704
Sebodroid <barnessebas99@gmail.com>
2023-03-24 18:34:59 -0400

    added userSchema and moongoose connection, .evn

--- Server/server.js
+++ Server/server.js
@@ -1,40 +1,53 @@
 import { ApolloServer, gql } from 'apollo-server'
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv'
+import { User } from './User.js';
 dotenv.config()
-//const typeDefs = './typeDefs';
-//const resolvers = './resolvers';
 
-const typeDefs = gql`
-  type Query {
-    userName: String,
-    message: String
-  }
-`;
-
-var myList = ["user102", "Hello world!!!"]
-
-const resolvers = {
-  Query: {
-    userName: () => myList[0], //Displays user102
-    message: () => myList[1] //Displays "Hello world!!!"
-  }
-};
-
-const username = encodeURIComponent("<userName>");
-
-const password = encodeURIComponent("<password>");
-
-const DB_URL = "mongodb://userName:password@127.0.0.1:27017/dbName";
+const DB_URL = process.env.DBADDRESS;
 
 mongoose.connect(DB_URL, {
-  useNewUrlParser: true
-}).then(() => console.log("MongoDB connected")).catch((err) => console.log(err));;
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log("MongoDB connected")).catch((err) => console.log(err));
+
+run();
+
+async function run() {
+  const user = new User({ userName: "user104", email: "user104@hotmail.com", pwd: "user104Password!" });
+  await user.save();
+  console.log(user);
+}
+
+// const user103 = new userSchema({
+//   userName: "user103",
+//   email: "user103@hotmail.com",
+//   pwd: "user103Password!"
+// })
 
 // const server = new ApolloServer({ typeDefs, resolvers });
-// const serverInfo = await server.listen({ port: 9010 });
-// console.log(`Server runnin at ${process.env.UNAME}`)
-
 // mongoose.connection.once("open", () =>
-//   server.start(() => console.log("We make magic over at localhost:9010"))
+//   server.listen(() => console.log("We make magic over at localhost:9010"))
 // );
+
+
+
+//CODE REGARDING GQL
+// const typeDefs = gql`
+//   type Query {
+//     userName: String,
+//     message: String
+//   }
+// `;
+// var myList = ["user102", "Hello world!!!"]
+
+// const resolvers = {
+//   Query: {
+//     userName: () => myList[0], //Displays user102
+//     message: () => myList[1] //Displays "Hello world!!!"
+//   }
+// };
+
+// const server = new ApolloServer({ typeDefs, resolvers });
+// const serverInfo = await server.listen({ port: process.env.PORT });
+// console.log(`Server runnin at ${process.env.PORT}`)
