0c3a7864af8f4d6db7d3233daea429939567e96b
savagekyle <66024623+savagekyle@users.noreply.github.com>
2023-04-07 13:31:05 -0400

    Sidebar layout change| added functional search bar

--- coolest-app/src/components/Profile.jsx
+++ coolest-app/src/components/Profile.jsx
@@ -1,24 +1,47 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
+import { tokens } from "../theme";
 
 const Profile = (props) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const hover = {
+    "&:hover": {
+      backgroundColor: `${colors.primary[900]}`,
+      cursor: "pointer",
+    },
+  };
+
   return (
-    <Box display="flex" flexDirection="column" gap="10px" marginBottom="20px">
+    <Box
+      display="flex"
+      gap="10px"
+      alignItems="center"
+      padding="20px"
+      sx={hover}
+    >
       <img
         src={props.img}
         alt={`${props.username} PFP`}
         style={{
-          height: !props.collapsed ? "80px" : "60px",
-          width: !props.collapsed ? "80px" : "60px",
+          height: !props.collapsed ? "70px" : "60px",
+          width: !props.collapsed ? "70px" : "60px",
           borderRadius: "50%",
-          marginInline: "auto",
-          cursor: "pointer",
+          textAlign: "center",
         }}
       />
       {!props.collapsed && (
-        <Typography variant="h6" textAlign="center" maxWidth="100px">
-          {props.username}
-        </Typography>
+        <Box lineHeight="1.6">
+          <Typography
+            variant="h6"
+            maxWidth="100px"
+            color={colors.blueAccent[500]}
+          >
+            {props.username}
+          </Typography>
+          <p style={{ fontSize: "12px" }}>{props.message}</p>
+        </Box>
       )}
     </Box>
   );
--- coolest-app/src/components/Sidebar.jsx
+++ coolest-app/src/components/Sidebar.jsx
@@ -1,8 +1,15 @@
 import React, { useState } from "react";
-import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import {
+  Box,
+  IconButton,
+  Typography,
+  useTheme,
+  InputBase,
+} from "@mui/material";
 import { tokens } from "../theme";
 import KeyboardReturnIcon from "@mui/icons-material/KeyboardReturn";
 import KeyboardTabIcon from "@mui/icons-material/KeyboardTab";
+import SearchIcon from "@mui/icons-material/Search";
 import Profile from "./Profile";
 import robot from "../assets/robot.jpg";
 import pfp from "../assets/pfp-placeholder.jpg";
@@ -12,22 +19,31 @@ const Sidebar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const [collapsed, setCollapsed] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
 
   //   For Testing Purposes
   const profileData = [
     {
       img: robot,
       username: "Billy Bob Joe",
+      message: "Hey there bro",
     },
     {
       img: pfp,
       username: "Mr. Savage",
+      message: "You're such a Savage",
     },
     {
       img: kebo,
       username: "Lil Peep",
+      message: "I'm not dead mother fucker",
     },
   ];
+
+  const filteredProfileData = profileData.filter(
+    (item) =>
+      item.username.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1
+  );
 
   const handleCollapse = () => {
     setCollapsed(!collapsed);
@@ -35,22 +51,48 @@ const Sidebar = () => {
 
   return (
     <Box
-      paddingInline="20px"
       height={`calc(100vh - 78px)`}
       borderRight={`1px solid ${colors.greenAccent[800]}`}
       marginRight="20px"
       position="relative"
     >
-      {/* Example of how we could utilize props for sidebar */}
-      {profileData.map((item) => {
-        return (
-          <Profile
-            img={item.img}
-            username={item.username}
-            collapsed={collapsed}
+      {!collapsed && (
+        <Box
+          display="flex"
+          backgroundColor={colors.primary[400]}
+          borderRadius="3px"
+          m="0 20px 20px"
+        >
+          <InputBase
+            sx={{ ml: 2, flex: 1 }}
+            placeholder="Search"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
           />
-        );
-      })}
+          <IconButton type="button" sx={{ p: 1 }}>
+            <SearchIcon />
+          </IconButton>
+        </Box>
+      )}
+      {/* Example of how we could utilize props for sidebar */}
+
+      {filteredProfileData.length === 0 ? (
+        <Typography variant="h5" textAlign="center">
+          User Not Found
+        </Typography>
+      ) : (
+        filteredProfileData.map((item) => {
+          return (
+            <Profile
+              key={item.username}
+              img={item.img}
+              username={item.username}
+              message={item.message}
+              collapsed={collapsed}
+            />
+          );
+        })
+      )}
 
       {/* Collapse and uncollapse sidebar */}
       {/* On collapse, text should be hidden */}
